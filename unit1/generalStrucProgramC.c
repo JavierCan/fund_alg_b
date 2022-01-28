57a91eeb23766eb6c3c35ce0316252e0c0209a18
JavierCan <2109022@upy.edu.mx>
2022-01-28 09:47:39 -0600

    estructura general c

---
+++ unit1/generalStrucProgramC.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<stdlib.h>
+//Global variables;
+//create a variable: datatype namevariable= anydata; 
+// DataTypes in c/c++/c#/php;
+int UnEntero = 3;
+float undecimal = 3.1416;
+char* name = "javiercan";
+char uncaracter ='M';
+//////////////////////////7//////////
+
+
+///ejecucion del programa///
+int main(){
+    printf ("hello Data B %s\n", name);
+    return 0;
+}
