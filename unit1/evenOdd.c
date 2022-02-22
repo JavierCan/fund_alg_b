db77f0b971c1009a359daaa66de656d2834f60d0
JavierCan <2109022@upy.edu.mx>
2022-02-22 10:35:39 -0600

    evenodd program new

--- unit1/evenOdd.c
+++ unit1/evenOdd.c
@@ -1,36 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h>
 
 
+int main(int argc, char ** argv){
 
-int main(int argc, char **argv){
+    int n;
+    scanf ("%d", &n);
+    bool notHasReminder = ( n % 2 == 0);
 
-
-    if (argc != 2){
-
-    printf("error program: u need o insert a number , e.g : ./%s argv[1]\n", argv[0]);
-    return 0;
-    }
-
-    bool isanumber = (argv[1][0] >= 48 && argv[1][0]  <=57); 
-
-    if  (!isanumber){
-
-        printf("error program : U need to insert just  a numbe not a letter, e.g: %s 8\n", argv [0]);
-        return 0;
-    }
-
-    int numbertotest = atoi(argv[1]);
-    int result = numbertotest % 2;
-    bool hasModule = ( result > 0 );
-
-    if (hasModule){
-        printf("impar\n");
-        
+    if (notHasReminder){
+        printf("the number %d is even \n", n);
     } else {
-        printf("par\n");
+        printf("the number %d is odd \n", n);
     }
     
-   return 0;
+    return 0;
 }
