989280683c4f2e03679090de1af3fd54947e5f2c
JavierCan <2109022@upy.edu.mx>
2022-03-04 08:47:45 -0600

    loops

--- unit2/loops.c
+++ unit2/loops.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "../libraries/utils.h"
+#include <stdlib.h>
 
 
 int main( int argc, char ** argv){
@@ -9,8 +10,36 @@ for ( int iterator = 1 ; iterator <=10 ; iterator = iterator +1){
     sleep (1000);
 
 }
+int rango = 100;
+int count = 0 ;
+while (rango > 1){
+    count = count + rango ;
+    rango-- ;
+}
+printf ("suma : %d\n", count);
 
 
+while (true) {
+ system ("clear");
+printf ("program running....\n");
+    //sleep (1000);
+    break;
+    
+};
+for (;;) {
+    break;
+}
+
+bool condicion ;
+int i = 1; 
+int cuenta = 0;
+do{
+    cuenta += i;
+printf("dato : %d\n", cuenta);
+condicion =(i<3);
+i++;
+
+}while(condicion);
 
 return 0;
 }
